9eb9ae36192608fecdcff426d4451d11b3fcf1fb
Lachlan McKinnie <89623046+Lachiemckbioinfo@users.noreply.github.com>
2023-10-19 15:23:48 +1000

    Add files via upload
    Changed sys.argv input to Argparse

--- fasta_extractor.py
+++ fasta_extractor.py
@@ -1,19 +1,61 @@
 from Bio import SeqIO
-import sys
+#import sys
+import argparse
+
+#Program details
+progname = "Fasta-Extractor"
+github = "https://github.com/Lachiemckbioinfo/fasta_extractor"
+
+#Initiate argparse
+parser = argparse.ArgumentParser(
+    prog = f'{progname}',
+    description = 'This program uses BioPython to extract fasta sequences from a multifasta using a list of sequence names',
+    epilog = f'Thank you for using {progname}. For more details, please visit the GitHub repository at {github}'
+)
+#Argparse arguments
+#Input file (list of gene names)
+parser.add_argument("-i", "--input",
+                    required=True,
+                    type = argparse.FileType('r'),
+                    metavar = '',
+                    dest = "genelist",
+                    help = 'Input file containing sequence list. Sequence names must be one name per line')
+
+parser.add_argument('-f', "--fasta",
+                    dest = 'infile',
+                    metavar = '',
+                    help = 'Input fasta file',
+                    required=True)
+
+parser.add_argument("-o", "--out",
+                    required=True,
+                    metavar = '',
+                    type = argparse.FileType('w'),
+                    dest = "outfile",
+                    help = "Output file name.")
+
+
+#Parse argparse parser parse
+args = parser.parse_args()
+genelist_file = args.genelist
+infile = args.infile
+outfile = args.outfile
+outfile_name = outfile.name
+
 
 #Input files
-genelist_file = sys.argv[1]
-input_fasta = sys.argv[2]
-output_fasta = sys.argv[3]
+#genelist_file = sys.argv[1]
+#input_fasta = sys.argv[2]
+#output_fasta = sys.argv[3]
 
 #ARead genelist_file and append lines to genelist
 genelist = []
-with open(genelist_file) as file:
-    while line := file.readline():
+with genelist_file:
+    while line := genelist_file.readline():
         genelist.append(line.rstrip())
 
 
-def extract_sequences(genelist, input_fasta, output_fasta):
+def extract_sequences(genelist, infile, outfile):
     #Start counters
     genecount = len(genelist)
     genes_found = 0
@@ -21,14 +63,14 @@ def extract_sequences(genelist, input_fasta, output_fasta):
     genes_parsed = []
     missing_genes = []
     #Open file and extract sequences
-    with open(output_fasta, "w") as outfile:
-        for record in SeqIO.parse(input_fasta, "fasta"):
+    with outfile:
+        for record in SeqIO.parse(infile, "fasta"):
             genes_parsed.append(record.id)
             if record.id in genelist:
                 SeqIO.write(record, outfile, "fasta")
                 genelist.remove(record.id)
                 genes_found += 1
-                print(f"Gene {record.id} extracted and saved to {output_fasta}")
+                print(f"Gene {record.id} extracted and saved to {outfile_name}")
                 
                 #Break loop when all sequences are found
                 if not genelist:
@@ -43,8 +85,8 @@ def extract_sequences(genelist, input_fasta, output_fasta):
 
 
 #Perform fasta sequence extraction
-genecount, genes_found, missing_genes = extract_sequences(genelist, input_fasta, output_fasta)
-print(f"Extracted {genes_found} fasta sequences from {input_fasta} and saved them to {output_fasta}.\n")
+genecount, genes_found, missing_genes = extract_sequences(genelist, infile, outfile)
+print(f"Extracted {genes_found} fasta sequences from {infile} and saved them to {outfile_name}.\n")
 #Return missing genes
 if len(missing_genes) > 0:
     print(f"{len(missing_genes)} fasta sequences were not found. Fasta sequences not found:")
